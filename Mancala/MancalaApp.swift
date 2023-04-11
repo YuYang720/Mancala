d6094174885949648df5a554042d4ac0f4810137
YuYanw Wang <yyweric0720@gmail.com>
2023-04-11 10:14:27 +0800

    Initial Commit

---
+++ Mancala/MancalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MancalaApp.swift
+//  Mancala
+//
+//  Created by User06 on 2023/4/11.
+//
+
+import SwiftUI
+
+@main
+struct MancalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
